66032c5a58f4fed9c9a4778cf80a702175c1c675
Joajy <joajy98@gmail.com>
2024-02-01 23:46:12 +0900

    week2 : [1225]암호생성기/Kim-Jae-Yoon

--- week2/[1225]암호생성기/Kim-Jae-Yoon/Main.java
+++ week2/[1225]암호생성기/Kim-Jae-Yoon/Main.java
@@ -11,6 +11,14 @@ public class Solution {
     static void solution(){
         int val = 1;
         idx = 0;
+        /**
+         * 귀찮 + 시간 여유로워 생략했지만,
+         * 이 문제의 경우 1~5 사이클을 반복하게 되는데
+         * 8개의 배열에서 15의 배수만큼씩 뺄 수 있는 만큼 빼고 시작하면
+         * 훨씬 속도 측면에서 유리하다.
+         * int q = min(a) / 15 + 1;
+         * each a[] -= q * 15;
+         */
         while(true){
             a[idx] -= val++;
             if(a[idx] <= 0){
