4d855f0bf5010797be78bf6c19c4f5fe5e876756
Marvin-Choi <90016581+Marvin-Choi@users.noreply.github.com>
2024-02-05 15:14:51 +0900

    Update and rename JUN14500_테트로미노_최민혁.java to Choi-Min-Hyuk.java

--- week2/[14500]테트로미노/Choi-Min-Hyuk/Choi-Min-Hyuk.java
+++ week2/[14500]테트로미노/Choi-Min-Hyuk/Choi-Min-Hyuk.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class JUN14500_테트로미노_최민혁 {
+public class BOJ14500_테트로미노_최민혁 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int N, M;
