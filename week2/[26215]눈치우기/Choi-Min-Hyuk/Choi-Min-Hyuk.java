023b2f73ee07114f36599105b0410874aa6dff6a
Marvin-Choi <90016581+Marvin-Choi@users.noreply.github.com>
2024-02-05 15:15:15 +0900

    Update and rename JUN26215_눈치우기_최민혁.java to Choi-Min-Hyuk.java

--- week2/[26215]눈치우기/Choi-Min-Hyuk/Choi-Min-Hyuk.java
+++ week2/[26215]눈치우기/Choi-Min-Hyuk/Choi-Min-Hyuk.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class JUN26215_눈치우기_최민혁 {
+public class BOJ26215_눈치우기_최민혁 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int N;
