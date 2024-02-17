91015df60d10126c172709edccda275bc01015cb
Marvin-Choi <90016581+Marvin-Choi@users.noreply.github.com>
2024-02-17 16:44:39 +0900

    Update Choi-Min-Hyuk.java

--- week4/[17070]파이프옮기기1/Choi-Min-Hyuk.java
+++ week4/[17070]파이프옮기기1/Choi-Min-Hyuk.java
@@ -3,45 +3,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
  
 public class BOJ17070_파이프_옮기기_1 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N, map[][], answer;
     
     // DP로 푸는 경우
     public static void main(String[] args) throws Exception {
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		int[][][] dp = new int[N][N][3]; // 0: 가로, 1: 세로, 2: 대각
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        int[][][] dp = new int[N][N][3]; // 0: 가로, 1: 세로, 2: 대각
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		dp[0][1][0] = 1;
+        dp[0][1][0] = 1;
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 2; j < N; j++) {
-				// 가로
-				if (j - 1 >= 0 && map[i][j] == 0)
-					dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+        for (int i = 0; i < N; i++) {
+            for (int j = 2; j < N; j++) {
+                // 가로
+                if (j - 1 >= 0 && map[i][j] == 0)
+                    dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
 
-				// 세로
-				if (i - 1 >= 0 && map[i][j] == 0)
-					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                // 세로
+                if (i - 1 >= 0 && map[i][j] == 0)
+                    dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
 
-				// 대각선
-				if (i - 1 >= 0 && j - 1 >= 0 && map[i][j] == 0 && map[i - 1][j] == 0 && map[i][j - 1] == 0)
-					dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
-			}
-		}
+                // 대각선
+                if (i - 1 >= 0 && j - 1 >= 0 && map[i][j] == 0 && map[i - 1][j] == 0 && map[i][j - 1] == 0)
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
+        }
 
-		answer = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
-		System.out.print(answer);
-	}
-    
+        answer = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+        System.out.print(answer);
+    }
+
+	
     // DFS로 푸는 경우
     public static void main2(String[] args) throws Exception {
         N = Integer.parseInt(br.readLine());
@@ -61,28 +62,28 @@ public class BOJ17070_파이프_옮기기_1 {
     
     public static void DFS(int r, int c, int direction) {
         if (r == N - 1 && c == N - 1) {
-        	answer++;
+            answer++;
             return;
         }
         
         // 파이프가 가로로 놓여졌을 때 오른쪽으로 가는 경우
         if (direction == 0) {
-        	if (c + 1 < N && map[r][c + 1] == 0)
-        		DFS(r, c + 1, 0);
+            if (c + 1 < N && map[r][c + 1] == 0)
+                DFS(r, c + 1, 0);
         }
         
         // 파이프가 세로로 놓여졌을 때 아래쪽으로 가는 경우
         if (direction == 1) {
-        	if (r + 1 < N && map[r + 1][c] == 0)
+            if (r + 1 < N && map[r + 1][c] == 0)
                 DFS(r + 1, c, 1);
         }
         
         // 파이프가 대각선으로 놓여졌을 때
         if (direction == 2) {
-        	// 오른쪽으로 가는 경우
-        	if (c + 1 < N && map[r][c + 1] == 0)
+            // 오른쪽으로 가는 경우
+            if (c + 1 < N && map[r][c + 1] == 0)
                 DFS(r, c + 1, 0);
-        	// 아래쪽으로 가는 경우
+            // 아래쪽으로 가는 경우
             if (r + 1 < N && map[r + 1][c] == 0)
                 DFS(r + 1, c, 1);
         }
