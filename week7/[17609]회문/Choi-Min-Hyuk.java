3c9d8c5edd34f9955d691de2c53c1affcbefb1e0
최민혁 <90016581+Marvin-Choi@users.noreply.github.com>
2024-03-09 17:38:51 +0900

    Update Choi-Min-Hyuk.java

--- week7/[17609]회문/Choi-Min-Hyuk.java
+++ week7/[17609]회문/Choi-Min-Hyuk.java
@@ -19,23 +19,24 @@ public class BOJ17609_회문_최민혁 {
 	}
 	
 	private static int getStringStatus(String s, int start, int end, int status) {
-	// 문자 삭제를 2번 하면 바로 return 2
-	if (status >= 2) {
-		return 2;
-	}
-	
-	// start와 end가 만나거나 지나치기 전까지 반복
-	while (start < end) {
-		// 같을 경우 포인터 한 칸씩 이동 진행
-		if (s.charAt(start) == s.charAt(end)) {
-			start++;
-			end--;
-		} 
+		// 문자 삭제를 2번 하면 바로 return 2
+		if (status >= 2) {
+			return 2;
+		}
 		
-		// 문자가 달라 하나를 삭제해야 하는 경우, 앞을 없앨지 뒤를 없앨지 고려하고 더 작은 값 선택
-		else
-			return Math.min(getStringStatus(s, start + 1, end, status + 1), getStringStatus(s, start, end - 1, status + 1));
-	}
-	return status;
+		// start와 end가 만나거나 지나치기 전까지 반복
+		while (start < end) {
+			// 같을 경우 포인터 한 칸씩 이동 진행
+			if (s.charAt(start) == s.charAt(end)) {
+				start++;
+				end--;
+			} 
+			
+			// 문자가 달라 하나를 삭제해야 하는 경우, 앞을 없앨지 뒤를 없앨지 고려하고 더 작은 값 선택
+			else
+				return Math.min(getStringStatus(s, start + 1, end, status + 1), getStringStatus(s, start, end - 1, status + 1));
+		}
+		
+		return status;
 	}
 }
