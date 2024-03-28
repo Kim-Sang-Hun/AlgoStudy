ff0a130d14a27d53ec55411c7ca1b1b895ce5777
최민혁 <90016581+Marvin-Choi@users.noreply.github.com>
2024-03-28 14:30:35 +0900

    Update Choi-Min-Hyuk.java

--- week9/[1865]웜홀/Choi-Min-Hyuk.java
+++ week9/[1865]웜홀/Choi-Min-Hyuk.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 벨만 포드로 풀면 되는데 N개의 지점이 모두 연결되었다는 보장이 없다는 점만 조심하면 됨
+// 벨만 포드로 풀면 됨
 public class BOJ1865_웜홀_최민혁 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	static String lineSeparator = System.lineSeparator();
 	static int TC, N, M, W;
-	static ArrayList<Edge>[] adjList;
+	static ArrayList<Node>[] adjList;
 	static final int INF = 25000001;
 
-	static class Edge {
+	static class Node {
 		int end;
 		int cost;
 
-		public Edge(int end, int cost) {
+		public Node(int end, int cost) {
 			this.end = end;
 			this.cost = cost;
 		}
@@ -44,8 +44,8 @@ public class BOJ1865_웜홀_최민혁 {
 				int E = Integer.parseInt(st.nextToken());
 				int T = Integer.parseInt(st.nextToken());
 
-				adjList[S].add(new Edge(E, T));
-				adjList[E].add(new Edge(S, T));
+				adjList[S].add(new Node(E, T));
+				adjList[E].add(new Node(S, T));
 			}
 
 			for (int i = 0; i < W; i++) {
@@ -54,7 +54,7 @@ public class BOJ1865_웜홀_최민혁 {
 				int E = Integer.parseInt(st.nextToken());
 				int T = -1 * Integer.parseInt(st.nextToken());
 
-				adjList[S].add(new Edge(E, T));
+				adjList[S].add(new Node(E, T));
 			}
 			
 			String result = BellmanFord() ? "NO" : "YES";
@@ -69,19 +69,20 @@ public class BOJ1865_웜홀_최민혁 {
 		Arrays.fill(dist, INF);
 		dist[1] = 0;
 		
-		for (int start = 1; start <= N; start++) {
-			for (int vertexIndex = 1; vertexIndex <= N; vertexIndex++) {
-				int edgeCount = adjList[vertexIndex].size();
+		// 1번 지점부터 N-1번 업데이트 이후 마지막 N번째 for문에서 거리가 업데이트 되면 가중치가 줄어드는 음수 사이클이 있음
+		for (int i= 1; i <= N; i++) {
+			for (int NodeIndex = 1; NodeIndex <= N; NodeIndex++) {
+				int edgeCount = adjList[NodeIndex].size();
 				
 				for (int edgeIndex = 0; edgeIndex < edgeCount; edgeIndex++) {
-					int from = vertexIndex;
-					int to = adjList[vertexIndex].get(edgeIndex).end;
-					int cost = adjList[vertexIndex].get(edgeIndex).cost;
+					int from = NodeIndex;
+					int to = adjList[NodeIndex].get(edgeIndex).end;
+					int cost = adjList[NodeIndex].get(edgeIndex).cost;
 					
 					if (dist[to] > dist[from] + cost) {
 						dist[to] = dist[from] + cost;
 						
-						if (start == N)
+						if (i == N)
 							return false;
 					}
 				}
