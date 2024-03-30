5514aa153f0f11693c7a2c5148158a42baf0d60e
DongHyun222 <90547091+DongHyun222@users.noreply.github.com>
2024-03-30 17:58:20 +0900

    week9/ [CT]루돌프의 반란/ Go-Dong-Hyun

---
+++ week9/[CT]루돌프의 반란/Go-Dong-Hyun.java
@@ -0,0 +1,25 @@
+package week08;
+
+public class RudolphDrawing {
+    public static void main(String[] args) {
+        drawRudolph();
+    }
+
+    public static void drawRudolph() {
+        System.out.println("           ___");
+        System.out.println("          /... .\\");
+        System.out.println("         (   o o   )");
+        System.out.println("         {    \"    }");
+        System.out.println("         |   ___  |");
+        System.out.println("          \\_______/");
+        System.out.println("        /           \\");
+        System.out.println("      /   /   |   |   \\   \\");
+        System.out.println("    /   /   /|   |\\   \\   \\");
+        System.out.println("  /   /   / / |   | \\   \\   \\");
+        System.out.println("|   /   / /   |   |   \\   \\   |");
+        System.out.println("|  /   / /|   |   |\\   \\   \\  |");
+        System.out.println("| |   / / |   |   | \\   \\   | |");
+        System.out.println("| |   |   |   |   |   |   | |");
+        System.out.println("|_|__|   |___|___|   |___|_|");
+    }
+}
