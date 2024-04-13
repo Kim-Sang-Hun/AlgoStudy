17f529cd2f72e7ac7186ef10a931c3c15148c3ed
최민혁 <90016581+Marvin-Choi@users.noreply.github.com>
2024-04-13 13:07:15 +0900

    week11: [10597]순열장난/Choi-Min-Hyuk.java
    백준 10597번 순열 장난 문제 풀이
    https://www.acmicpc.net/problem/14675

---
+++ week11/[14675]단절점과 단절선/Choi-Min-Hyuk.java
@@ -0,0 +1,47 @@
+package week11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14675_단절점과단절선_최민혁 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static String lineSeparator = System.lineSeparator();
+	static int N, q, adjNodeCounts[];
+	
+	public static void main(String[] args) throws Exception {
+		N = Integer.parseInt(br.readLine());
+		adjNodeCounts = new int[N + 1];
+		
+		for (int i = 1; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adjNodeCounts[a]++;
+			adjNodeCounts[b]++;
+		}
+		
+		q = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			if (t == 1) {
+				if (adjNodeCounts[k] > 1)
+					sb.append("yes").append(lineSeparator);
+				else
+					sb.append("no").append(lineSeparator);
+			}
+			
+			else if (t == 2) {
+				sb.append("yes").append(lineSeparator);
+			}
+		}
+		
+		System.out.print(sb);
+	}
+}
